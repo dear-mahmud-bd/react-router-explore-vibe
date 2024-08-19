fadf7cc72844122823cf664eb8ece36242ea607a
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2024-08-19 22:44:36 +0600

    sorting functionality

--- src/components/ListedBooks/ListedBook/ListedBook.jsx
+++ src/components/ListedBooks/ListedBook/ListedBook.jsx
@@ -4,33 +4,34 @@ import PropTypes from 'prop-types';
 import { FaRegCalendar } from 'react-icons/fa';
 import { FiUsers } from 'react-icons/fi';
 import { FaRegFileLines } from 'react-icons/fa6';
+import { NavLink } from 'react-router-dom';
 
-const ListedBook = ({book}) => {
-    const { bookId, image, bookName, author, tags, yearOfPublishing,publisher,totalPages, category, rating } = book;
+const ListedBook = ({ book }) => {
+    const { bookId, image, bookName, author, tags, yearOfPublishing, publisher, totalPages, category, rating } = book;
     return (
         <div>
-            <div className="hero-content flex justify-start items-start border rounded-lg my-5">
-                <figure className="p-10 bg-gray-200 mr-6 rounded-xl">
-                    <img src={image} className="rounded-lg shadow-2xl h-60 w-auto" />
+            <div className="hero-content flex flex-col md:flex-row justify-start items-center border rounded-lg my-5 w-full">
+                <figure className=" md:p-10 bg-gray-200 md:mr-6 rounded-xl mx-auto md:mx-0">
+                    <img src={image} className="rounded-lg shadow-2xl sm:h-[350px] w-60 sm:w-[350px]" />
                 </figure>
-                <div className='space-y-5'>
+                <div className='space-y-5 w-full flex-grow'>
                     <h2 className="text-xl font-bold mb-2">{bookName}</h2>
                     <p className="text-gray-700 font-semibold mb-2">By : {author}</p>
-                    <div className="flex gap-4 mb-4 font-semibold">
+                    <div className="flex gap-2 mb-4 font-semibold items-center">
                         <span className="text-lg font-bold">Tag</span>
-                        <span className="px-4 py-2 bg-green-100 rounded-full text-customGreen">#{tags[0]}</span>
-                        <span className="px-4 py-2 bg-green-100 rounded-full text-customGreen">#{tags[1]}</span>
-                        <span className="px-4 py-2 flex gap-2 items-center text-gray-500"><FaRegCalendar />Year of Publishing: {yearOfPublishing}</span>
+                        <span className="px-3 py-2 bg-green-100 rounded-full text-customGreen ">#{tags[0]}</span>
+                        <span className="px-3 py-2 bg-green-100 rounded-full text-customGreen ">#{tags[1]}</span>
+                        <span className="px-3 py-2 flex gap-2 items-center text-gray-500"><FaRegCalendar />Year of Publishing: {yearOfPublishing}</span>
                     </div>
-                    <div className="flex gap-4 mb-4 text-gray-600 font-semibold">
-                        <span className="px-4 py-2 flex gap-2 items-center"><FiUsers /> Publisher: {publisher}</span>
-                        <span className="px-4 py-2 flex gap-2 items-center"><FaRegFileLines /> Page {totalPages}</span>
-                    </div>
-                    <div className="flex gap-4 border-t pt-5">
-                        <span className="px-4 py-2 bg-blue-100 rounded-full text-blue-500">Category: {category}</span>
-                        <span className="px-4 py-2 bg-yellow-100 rounded-full text-yellow-500">Rating: {rating}</span>
-                        <span className="px-4 py-2 bg-customGreen rounded-full text-white">View Details</span>
-                    </div>
+                    <p className="flex gap-2 mb-4 text-gray-600 font-semibold">
+                        <span className="px-3 py-2 flex gap-2 items-center"><FiUsers /> Publisher: {publisher}</span>
+                        <span className="px-3 py-2 flex gap-2 items-center"><FaRegFileLines /> Page {totalPages}</span>
+                    </p>
+                    <p className="flex gap-2 border-t pt-5">
+                        <span className="px-3 py-2 bg-blue-100 rounded-full text-blue-500">Category: {category}</span>
+                        <span className="px-3 py-2 bg-yellow-100 rounded-full text-yellow-500">Rating: {rating}</span>
+                        <NavLink to={`/all-books/${bookId}`} className="px-3 py-2 bg-customGreen rounded-full text-white">View Details</NavLink>
+                    </p>
                 </div>
             </div>
         </div>
--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,42 +1,66 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ListedBook from './ListedBook/ListedBook';
 import { getMatchedBooks } from '../utility/useLocalStorage';
 import useFetchBooks from '../utility/useFetchBooks';
 
 const ListedBooks = () => {
+    const { data: books } = useFetchBooks('books.json');
 
-    const { data: books, loading } = useFetchBooks('books.json');
+    const [readBooks, setReadBooks] = useState([]);
+    const [wishlistBooks, setWishlistBooks] = useState([]);
 
-    const readBooks = getMatchedBooks(books, "Read");
-    const wishlistBooks = getMatchedBooks(books, "Wishlist");
+    useEffect(() => {
+        if (books) {
+            setReadBooks(getMatchedBooks(books, "Read"));
+            setWishlistBooks(getMatchedBooks(books, "Wishlist"));
+        }
+    }, [books]);
 
-    console.log(readBooks);
+
+    const sortBooks = (books, field) => {
+        return [...books].sort((a, b) => b[field] - a[field]);
+    };
+    const setState = (criteria) => {
+        setReadBooks(sortBooks(readBooks, criteria));
+        setWishlistBooks(sortBooks(wishlistBooks, criteria));
+    };
+    
+    const handleSortChange = (e) => {
+        const criteria = e.target.value;
+        if (criteria === 'rating') {
+            setState(criteria);
+        } else if (criteria === 'totalPages') {
+            setState(criteria);
+        } else if (criteria === 'yearOfPublishing') {
+            setState(criteria);
+        }
+    };
+
     return (
         <div>
             <div className='my-1 py-5 bg-gray-200 rounded-lg'>
                 <h1 className='text-center text-4xl font-bold'>Books</h1>
             </div>
 
-            <div className='my-10 flex justify-center'>
-                <select className="select select-bordered w-full max-w-xs bg-customGreen font-bold text-white border-0">
-                    <option disabled defaultValue>Sort By</option>
-                    <option className='bg-gray-200 text-black'>Default</option>
-                    <option className='bg-gray-200 text-black'>Rating</option>
-                    <option className='bg-gray-200 text-black'>Number of Pages</option>
-                    <option className='bg-gray-200 text-black'>Publisher Year</option>
+            <div className='my-10 flex justify-center items-center'>
+                <select onChange={handleSortChange} className="select select-bordered w-full max-w-xs bg-customGreen font-bold text-white border-0 text-center">
+                    <option className='bg-gray-200 text-black' defaultValue>Sort By</option>
+                    <option value="rating" className='bg-gray-200 text-black'>Rating</option>
+                    <option value="totalPages" className='bg-gray-200 text-black'>Number of Pages</option>
+                    <option value="yearOfPublishing" className='bg-gray-200 text-black'>Publisher Year</option>
                 </select>
             </div>
 
             <div role="tablist" className="tabs tabs-lifted border-l-0">
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" defaultChecked />
+                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read_Books" defaultChecked />
                 <div role="tabpanel" className="tab-content bg-base-100 border-b-0 border-x-0 border-base-300 p-1">
                     {
                         readBooks.map((book, idx) => <ListedBook key={idx} book={book}></ListedBook>)
                     }
                 </div>
 
-                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" />
+                <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist_Books" />
                 <div role="tabpanel" className="tab-content bg-base-100 border-b-0 border-x-0 border-base-300 p-1">
                     {
                         wishlistBooks.map((book, idx) => <ListedBook key={idx} book={book}></ListedBook>)
